ef4d3e2233b44e385b1c28d93207f90dc8e99262
testrocket <rocketplusplus@gmail.com>
2014-03-17 22:24:26 +0100

    unittest update

--- calc/tests/test_parser.py
+++ calc/tests/test_parser.py
@@ -4,12 +4,9 @@ from calc.main import parser
 
 class TestParser(unittest.TestCase):
     def setUp(self):
-        self.expr = "1 + 2 * (2 - 1)"
+        self.expr = "a + 2 * sin(2 - 1) + b"
 
     def test_parse1(self):
         result = str(parser.parse_rpn(self.expr))
-        assert result == "[1, 2, 2, 1, -, *, +]", "Incorrect parse result: %s" % result
+        assert result == "[a, 2, 2, 1, -, sin, *, +, b, +]", "Incorrect parse result: %s" % result
 
-    def test_parse2(self):
-        result = str(parser.parse_rpn(self.expr))
-        assert result != "[1, 2, 2, 1, *, -, +]", "Incorrect parse result: %s" % result
