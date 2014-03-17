1402b8fa2f4bb8fbc75ee48bf5f90fc628c599bd
testrocket <rocketplusplus@gmail.com>
2014-03-17 22:45:21 +0100

    unit tests for calculate function

---
+++ calc/tests/test_calc.py
@@ -0,0 +1,27 @@
+import unittest
+
+from calc import calc
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_calculate1(self):
+        result = calc.calculate("1 + 2")
+        assert result == 3, "Incorrect calculation result: %s" % result
+
+    def test_calculate2(self):
+        result = calc.calculate("1 + 2 * 3")
+        assert result == 7, "Incorrect calculation result: %s" % result
+
+    def test_calculate3(self):
+        result = calc.calculate("(1 + 2) * 3")
+        assert result == 9, "Incorrect calculation result: %s" % result
+
+    def test_calculate4(self):
+        result = calc.calculate("1 + sin(rad(90))")
+        assert result == 2, "Incorrect calculation result: %s" % result
+
+    def test_calculate5(self):
+        result = calc.calculate("1 + cos(rad(90))")
+        assert result == 1, "Incorrect calculation result: %s" % result
