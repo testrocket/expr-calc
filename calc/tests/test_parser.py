c3af013ca90a103196436a4a42c9d0ec9337b2f4
testrocket <rocketplusplus@gmail.com>
2014-03-31 23:39:30 +0200

    added more tests for parser

--- calc/tests/test_parser.py
+++ calc/tests/test_parser.py
@@ -4,12 +4,20 @@ from calc.main import parser
 
 class TestParser(unittest.TestCase):
     def setUp(self):
-        self.expr = "a + 2 * sin(2 - 1) + b"
+        pass
 
     def test_parse1(self):
-        result = str(parser.parse_rpn(self.expr))
+        result = str(parser.parse_rpn("a + 2 * sin(2 - 1) + b"))
         assert result == "[a, 2, 2, 1, -, sin, *, +, b, +]", "Incorrect parse result: %s" % result
 
     def test_parse2(self):
         result = str(parser.parse_rpn("a = 1 + 2 * 4"))
         assert result == "[a, 1, 2, 4, *, +, =]", "Incorrect parse result: %s" % result
+
+    def test_parse3(self):
+        result = str(parser.parse_rpn("a = cos( 2 + 4) * 2"))
+        assert result == "[a, 2, 4, +, cos, 2, *, =]", "Incorrect parse result: %s" % result
+
+    def test_parse4(self):
+        result = str(parser.parse_rpn("a = cos(2 + 4) ^ 2 + 2"))
+        assert result == "[a, 2, 4, +, cos, 2, ^, 2, +, =]", "Incorrect parse result: %s" % result
